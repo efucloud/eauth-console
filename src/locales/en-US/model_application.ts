10ab09640f755624ea440502c3aef70a4fbafb24
cloudy <272685110@qq.com>
2026-03-30 15:05:41 +0800

    feat: 应用翻译

--- src/locales/en-US/model_application.ts
+++ src/locales/en-US/model_application.ts
@@ -5,17 +5,20 @@ export default {
   'model.application.create': 'Create Application',
   'model.application.name': 'Application Name',
   'model.application.quickAccess': 'Quick Access',
-  'model.application.quickAccess.description': 'The application will appear in the top-left corner of the system',
+  'model.application.quickAccess.description':
+    'The application will appear in the top-left corner of the system',
   'model.application.icon': 'Icon',
   'model.application.clientId': 'Client ID',
-  'model.application.clientId.tooltip': 'Client ID is the identity of the application. Please keep it secure.',
+  'model.application.clientId.tooltip':
+    'Client ID is the identity of the application. Please keep it secure.',
   'model.application.clientSecret': 'Client Secret',
   'model.application.clientSecret.tooltip':
     'Client Secret is the identity credential of the application. Please keep it secure.',
   'model.application.description': 'Application Description',
   'model.application.home': 'Homepage URL',
   'model.application.enable': 'Status',
-  'model.application.enable.description': 'Disabled applications cannot use authentication in this system',
+  'model.application.enable.description':
+    'Disabled applications cannot use authentication in this system',
   'model.application.owner': 'Application Administrator',
   'model.application.accounts': 'Application Members',
   'model.application.enable.enable': 'Enable',
@@ -32,4 +35,5 @@ export default {
   'pages.application.list.title': 'Applications',
   'model.application.certificate': 'Public Key',
   'model.application.certificate.tooltip': 'Public key is used to verify whether a token is valid',
+  'model.application.redirectUriMatchType': 'Callback Address Matching Method',
 };
